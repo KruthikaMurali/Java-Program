9e7f63a943bb5596e162e8bff12574c4d081d670
KruthikaMurali <89632252+KruthikaMurali@users.noreply.github.com>
2021-09-03 15:22:56 +0530

    Update eg of object and class.java

--- eg of object and class.java
+++ eg of object and class.java
@@ -4,7 +4,7 @@
 
     public static void main(String args[]) {
      Student1 s1=new Student1();//creating an object of Student  
-	  System.out.println(s1.id);  
+      System.out.println(s1.id);  
       System.out.println(s1.name);  
     }
 }
