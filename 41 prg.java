31b00db916352fa77a50d40749eef67c25eb0b74
KruthikaMurali <89632252+KruthikaMurali@users.noreply.github.com>
2021-09-17 15:53:07 +0530

    Create 41 prg.java

---
+++ 41 prg.java
@@ -0,0 +1,7 @@
+public class TestSubstring{  
+public static void main(String args[]){  
+	String s1="Java is a programming language. Java is a platform. Java is an Island.";    
+	String replaceString=s1.replace("Java","Kava");//replaces all occurrences of "Java" to "Kava"    
+	System.out.println(replaceString);  
+  }
+}
