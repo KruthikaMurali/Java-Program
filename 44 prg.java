669500ed74541d7c36d335d4ab93d7e5e91d1df7
KruthikaMurali <89632252+KruthikaMurali@users.noreply.github.com>
2021-09-24 15:07:35 +0530

    Create 44 prg.java

---
+++ 44 prg.java
@@ -0,0 +1,7 @@
+public class A{  
+	public static void main(String args[]){  
+	StringBuffer sb=new StringBuffer("Hello");  
+	sb.replace(1,3,"Java");  
+	System.out.println(sb);//prints HJavalo  
+	}  
+	} 
